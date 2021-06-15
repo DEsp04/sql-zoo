bd086586ce892a3a602dc6cb15368edd95a1a26d
David Espinal <despinal0425@gmail.com>
2021-06-15 17:20:50 -0400

    retrieve population from Germany

---
+++ practice.sql
@@ -0,0 +1,9 @@
+
+--Modify it to show the population of Germany
+1.
+SELECT population FROM world
+  WHERE name = 'Germany'
+
+
+-- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+2.

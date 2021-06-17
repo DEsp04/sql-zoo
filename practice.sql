d75c6d696ac6cafc60d63a624dbd90c97385b013
David Espinal <despinal0425@gmail.com>
2021-06-17 15:46:17 -0400

    find name of country with double 'o'

--- practice.sql
+++ practice.sql
@@ -15,3 +15,43 @@ SELECT name, population FROM world
 3.
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
+
+
+ 
+-- SECTION 1 - SELECT NAME
+ 
+ 
+--Find the country that start with Y
+ 
+1. SELECT name FROM world
+  WHERE name LIKE 'Y%'
+
+
+
+-- Find the countries that end with y
+2. SELECT name FROM world
+  WHERE name LIKE '%Y'
+
+
+-- Find the countries that contain the letter x
+3. SELECT name FROM world
+  WHERE name LIKE '%x%'
+
+
+-- Find the countries that end with land
+4. SELECT name FROM world
+  WHERE name LIKE '%land'
+
+  
+-- Find the countries that start with C and end with ia
+5. SELECT name FROM world
+  WHERE name LIKE 'C%ia'
+
+
+-- Find the country that has oo in the name
+6. SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+
+-- Find the countries that have three or more a in the name
+7.
